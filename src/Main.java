f77594e63716c80f90363bcd2ce3a4a5e05585e8
Juan Alberto Cuevas Juarez <136052531+juan-albertoC@users.noreply.github.com>
2024-05-19 23:02:48 -0600

    atributos de la clase Pelicula y sus metodos sin retorno void mostrar, evaluar y metodo con retorno double calcular

--- src/Main.java
+++ src/Main.java
@@ -11,22 +11,47 @@ public class Main {
         miMovie.nombre = "Duro de Matar";
         miMovie.fechaDeLanzamiento = 2007;
         miMovie.duracionEnMinutos = 125;
+        //anteriormente directamente
+        //System.out.println("Mi pelicula es: " + miMovie.nombre);
 
-        System.out.println("Mi pelicula es: " + miMovie.nombre);
-        System.out.println("Su fecha de lanzamiento es: " + miMovie.fechaDeLanzamiento);
-        System.out.println("Su duracion es: " + miMovie.duracionEnMinutos + "minutos");
 
+
+        miMovie.mostrarFichaTecnica();   //llamada del metodo mostrarFichaTecnica()
+        miMovie.evaluar(7.8);      //llamada del metodo evaluar() donde se agrega calificacion por parte del usuario
+        miMovie.evaluar(9.5);      //llamada del metodo evaluar() donde se agrega calificacion por parte del usuario
+        System.out.println(miMovie.sumaDeLasEvaluaciones);  //Imprime las sumas de las evaluaciones
+        System.out.println(miMovie.totalDeLasEvaluaciones); //Imprimeel total de las evaluciones
+        System.out.println(miMovie.calcularMedia());    //Imprime por pantalla la Llamada del metodo calcularMedia
+        System.out.println(miMovie);  //direccion de memoria unica del objeto
+
+
+
+
+//--------------------------------------------------------------------------------------------------------------------
         //Instancia 2
         Pelicula otraMovie = new Pelicula();
         otraMovie.nombre = "Harry potter piedra filosofal";
         otraMovie.fechaDeLanzamiento = 2001;
         otraMovie.duracionEnMinutos = 215;
 
-        System.out.println("Mi otra Movie es: " + otraMovie.nombre);
-        System.out.println("su fecha de lanzamiento de otra Movie es: " + otraMovie.fechaDeLanzamiento);
-        System.out.println("su duracion de otra Movie es: " + otraMovie.duracionEnMinutos + "minutos");
+        //anteriormente directamente
+        //System.out.println("Mi otra Movie es: " + otraMovie.nombre);
+        //System.out.println("su fecha de lanzamiento de otra Movie es: " + otraMovie.fechaDeLanzamiento);
+        //System.out.println("su duracion de otra Movie es: " + otraMovie.duracionEnMinutos + "minutos");
 
+
+
+        otraMovie.mostrarFichaTecnica();  //llamada del metodo mostrarFichaTecnica()
+        otraMovie.evaluar(10);
+        otraMovie.evaluar(9.8);
+        otraMovie.evaluar(10);
+        otraMovie.evaluar(10);
+        System.out.println(otraMovie.sumaDeLasEvaluaciones);
+        System.out.println(otraMovie.totalDeLasEvaluaciones);
+        System.out.println(otraMovie.calcularMedia());
         System.out.println(otraMovie);  //direccion de memoria unica del objeto
+
+
 
     }
 }
--- src/Pelicula.java
+++ src/Pelicula.java
@@ -5,10 +5,39 @@ POO
  */
 //Estructura de un objeto: clases modelo, en este caso clase Pelicula
 public class Pelicula {
-    //Atributos para diferentes peliculas u objetos
+    //--------------------------------------------Atributos para diferentes peliculas u objetos
     String nombre;
     int fechaDeLanzamiento;
     int duracionEnMinutos;
     boolean incluidoEnElPlan;
+    //nueva regla del negocio: evaluar las peliculas por lo que implica nueva variable
+    double sumaDeLasEvaluaciones;
+
+    int totalDeLasEvaluaciones;   //Atributo acumulativo de las sumatorias de las evaluaciones donde se incrementa cada vez que una persona evalua
+
+//------------------------------------------------------------------------------------------------------------
+    //METODOS VOID SIN RETORNO
+
+    //metodo para mostrar la ficha tecnica o informacion acerca de la pelicula
+    void mostrarFichaTecnica(){
+
+        System.out.println("El nombre de la pelicula es: " + nombre);
+        System.out.println("La fecha de lanzamiento es: " + fechaDeLanzamiento);
+        System.out.println("La duracion es: " + duracionEnMinutos + "minutos");
+
+    }
+
+    //metodo para evaluar la pelicula segun la nota, espera recibir un dato de tipo double
+    void evaluar(double nota){
+        sumaDeLasEvaluaciones += nota;
+        totalDeLasEvaluaciones++;  //variable que se incremnta cada vez que una persona evalua una pelicula
+    }
+
+    //METODO CON RETORNO
+    //metodo que retorna un valor de tipo double para calcular la media
+    double calcularMedia(){
+        return sumaDeLasEvaluaciones / totalDeLasEvaluaciones;
+    }
+
 }
 
