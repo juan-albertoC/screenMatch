e1c222fb505af78443f26ed22be68b6c90c0fb41
Juan Alberto Cuevas Juarez <136052531+juan-albertoC@users.noreply.github.com>
2024-05-20 23:06:04 -0600

    Modificador de Acceso Private y Refactorizacion de Codigo

--- src/Main.java
+++ src/Main.java
@@ -11,45 +11,45 @@ public class Main {
         miMovie.nombre = "Duro de Matar";
         miMovie.fechaDeLanzamiento = 2007;
         miMovie.duracionEnMinutos = 125;
-        //anteriormente directamente
+        //Anteriormente directamente en el main
         //System.out.println("Mi pelicula es: " + miMovie.nombre);
-
-
-
+        //Ahora con metodo mostrarFichaTecnica()
         miMovie.mostrarFichaTecnica();   //llamada del metodo mostrarFichaTecnica()
         miMovie.evaluar(7.8);      //llamada del metodo evaluar() donde se agrega calificacion por parte del usuario
         miMovie.evaluar(9.5);      //llamada del metodo evaluar() donde se agrega calificacion por parte del usuario
-        System.out.println(miMovie.sumaDeLasEvaluaciones);  //Imprime las sumas de las evaluaciones
-        System.out.println(miMovie.totalDeLasEvaluaciones); //Imprimeel total de las evaluciones
+        System.out.println(miMovie.getTotalDeLasEvaluaciones()); //Imprime y obtiene el total de las evaluaciones
         System.out.println(miMovie.calcularMedia());    //Imprime por pantalla la Llamada del metodo calcularMedia
         System.out.println(miMovie);  //direccion de memoria unica del objeto
 
+        //Restricciones para seguridad donde usuario no modifique directamente en el codigo, hace uso del concepto de encapsulamiento ()
+        //Usuario se abstrae (Abstraccion) del conocimiento detras de la aplicacion por ello hacer uso de los MODFICADORES DE ACCESO O VISIBILIDAD: private, public, restricted
 
 
 
-//--------------------------------------------------------------------------------------------------------------------
-        //Instancia 2
-        Pelicula otraMovie = new Pelicula();
-        otraMovie.nombre = "Harry potter piedra filosofal";
-        otraMovie.fechaDeLanzamiento = 2001;
-        otraMovie.duracionEnMinutos = 215;
 
-        //anteriormente directamente
+
+//----------------------------------------------REFACTORIZACION DE CODIGO----------------------------------------------------------------------
+        //Instancia 2 para otro objeto
+        //Pelicula otraMovie = new Pelicula();
+        //otraMovie.nombre = "Harry potter piedra filosofal";
+        //otraMovie.fechaDeLanzamiento = 2001;
+        //otraMovie.duracionEnMinutos = 215;
+
+        //Anteriormente directamente en el main
         //System.out.println("Mi otra Movie es: " + otraMovie.nombre);
         //System.out.println("su fecha de lanzamiento de otra Movie es: " + otraMovie.fechaDeLanzamiento);
         //System.out.println("su duracion de otra Movie es: " + otraMovie.duracionEnMinutos + "minutos");
-
-
-
-        otraMovie.mostrarFichaTecnica();  //llamada del metodo mostrarFichaTecnica()
-        otraMovie.evaluar(10);
-        otraMovie.evaluar(9.8);
-        otraMovie.evaluar(10);
-        otraMovie.evaluar(10);
-        System.out.println(otraMovie.sumaDeLasEvaluaciones);
-        System.out.println(otraMovie.totalDeLasEvaluaciones);
-        System.out.println(otraMovie.calcularMedia());
-        System.out.println(otraMovie);  //direccion de memoria unica del objeto
+        //Ahora con metodo mostrarFichaTecnica()
+        //otraMovie.mostrarFichaTecnica();  //llamada del metodo mostrarFichaTecnica()
+        //otraMovie.evaluar(10);
+        //otraMovie.evaluar(9.8);
+        //otraMovie.evaluar(10);
+        //otraMovie.evaluar(10);
+        //System.out.println(otraMovie.sumaDeLasEvaluaciones);      //se elimina esta linea que Imprime las sumas de las evaluaciones (Usuario no deberia ver)
+        //System.out.println(otraMovie.totalDeLasEvaluaciones);    //se cambia esta linea por getter creado
+        //System.out.println(otraMovie.getTotalDeLasEvaluaciones());  //Imprime y obtiene el total de las evaluaciones
+        //System.out.println(otraMovie.calcularMedia());
+        //System.out.println(otraMovie);  //direccion de memoria unica del objeto
 
 
 
--- src/Pelicula.java
+++ src/Pelicula.java
@@ -11,10 +11,15 @@ public class Pelicula {
     int duracionEnMinutos;
     boolean incluidoEnElPlan;
     //nueva regla del negocio: evaluar las peliculas por lo que implica nueva variable
-    double sumaDeLasEvaluaciones;
+    private double sumaDeLasEvaluaciones;  //hacer uso de los modificadores de acceso: Private, donde usuario no podra ni ver ni sobreescribir
 
-    int totalDeLasEvaluaciones;   //Atributo acumulativo de las sumatorias de las evaluaciones donde se incrementa cada vez que una persona evalua
+    private int totalDeLasEvaluaciones;   //Atributo acumulativo de las sumatorias de las evaluaciones donde se incrementa cada vez que una persona evalua
 
+    //regla de negocio o escenario para obtener (getters)
+    int getTotalDeLasEvaluaciones(){
+        //retornar el tipo de dato (usuario no consigue modificar un valor )
+        return totalDeLasEvaluaciones;
+    }
 //------------------------------------------------------------------------------------------------------------
     //METODOS VOID SIN RETORNO
 
