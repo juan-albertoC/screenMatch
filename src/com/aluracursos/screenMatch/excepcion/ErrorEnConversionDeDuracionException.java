d6f6ce483ccd47b8480af11846a0fe1adbfa8e78
Juan Alberto Cuevas Juarez <136052531+juan-albertoC@users.noreply.github.com>
2024-06-25 13:51:57 -0600

    Crear tus propios exceptions, en MainConBusqueda habia un error en la URI tras colocar top gun, por ello aplicar una metodologia de metodo replace del espacio por ser reemplazado por un signo de +, mejor utilizar clase URLEncoder los digitos de variable duracion son 2 cuando deberia ser 3 cambiar en substring(0,3) pero prueba con 2 hay un error,reemplaza ese espacio por nada despues de substring(0,3).replace(espacio, nada) otra pelicula marca error N/A entonces crear propia exception especifica a este caso en clase public Titulo(TituloOmdb miTituloOmdb) con un if despues de this.fechaDeLanzamiento if(miTituloOmdb.runtime().contains(N/A)){ throw new ErrorEnConversionDeDuracionException(No pude convertir la duracion..N/A)} crear la clase ErrorEnConversionDeDuracionException que extiende de RuntimeException en un nuevo paquete excepcion se tiene un mensaje privado sobreescribir getMessage() y retornar this.mensaje clase MainConBusqueda cambiar en el catch generico la nueva clase de error ErrorEnConversionDeDuracionException mandar a llamar e.getMessage() es mejor guardar en un file log los errores que han acontecido

--- src/com/aluracursos/screenMatch/excepcion/ErrorEnConversionDeDuracionException.java
+++ src/com/aluracursos/screenMatch/excepcion/ErrorEnConversionDeDuracionException.java
@@ -1,2 +1,18 @@
-package com.aluracursos.screenMatch.excepcion;public class ErrorEnConversionDeDuracionException {
+package com.aluracursos.screenMatch.excepcion;
+
+public class ErrorEnConversionDeDuracionException extends RuntimeException { //No extiende de Throwable sino de RuntimeException, ya que es no checkable
+
+    private String mensaje;
+
+    public ErrorEnConversionDeDuracionException(String mensaje) { //se cambio de nombre por String mensaje
+
+        this.mensaje = mensaje;
+    }
+
+    //sobreescribir
+    @Override
+    public String getMessage() {
+        //return super.getMessage(); forma parte de clase madre
+        return this.mensaje;  //retorne mensaje
+    }
 }
